a9d94270c871b06ba3f60640cdeaf642b862ea29
superkookai <weerawut@live.com>
2025-03-14 17:06:09 +0700

    Initial Commit

---
+++ SubcriberBootcamp/SubcriberBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubcriberBootcampApp.swift
+//  SubcriberBootcamp
+//
+//  Created by Weerawut Chaiyasomboon on 14/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct SubcriberBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
